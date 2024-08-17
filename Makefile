29bd52d49c727e6b2cf2762bd32cd8d6c168bc05
AardhynLavender <aardhyn@gmail.com>
2024-08-17 18:48:18 +1200

    feat: improve environment

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 .PHONY: all database database.down database.connect server client
 include .env
 
+DATABASE_URL=postgres://$(DATABASE_USER):$(DATABASE_PASSWORD)@$(DATABASE_DOMAIN):$(DATABASE_PORT)
+
 all:
 	make database \
 	&& make server;
@@ -16,7 +18,7 @@ database.connect:
 
 server:
 	cd server/repository \
-	&& diesel migration run \
+	&& diesel migration run --database-url $(DATABASE_URL) \
 	&& cd ../api \
 	&& cargo run;
 
--- server/repository/src/connection.rs
+++ server/repository/src/connection.rs
@@ -6,8 +6,11 @@ use std::env;
 pub fn establish_connection() -> Result<PgConnection, diesel::ConnectionError> {
   dotenvy::dotenv().expect("Failed to load .env file");
 
-  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  let host = env::var("DATABASE_HOST").expect("DATABASE_HOST must be set");
+  let port = env::var("DATABASE_PORT").expect("DATABASE_PORT must be set");
+  let user = env::var("DATABASE_USER").expect("DATABASE_USER must be set");
+  let password = env::var("DATABASE_PASSWORD").expect("DATABASE_PASSWORD must be set");
+  let connection_string = format!("postgres://{}:{}@{}:{}", user, password, host, port);
 
-  let connection = PgConnection::establish(&database_url);
-  connection
+  PgConnection::establish(&connection_string)
 }
