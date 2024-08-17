25dd8cf926778a1e296b071c7e23d95571c97947
AardhynLavender <aardhyn@gmail.com>
2024-08-17 18:52:27 +1200

    chore: type client environment

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /**
+   * The port the client is running on.
+   */
+  readonly VITE_CLIENT_PORT: string;
+  /**
+   * The root URL of the API.
+   */
+  readonly VITE_API_DOMAIN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
