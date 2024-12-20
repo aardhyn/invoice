284cea00b5fb37fe7c8b87c8f10deeeb5eb089ac
aardhynlavender <aardhyn@gmail.com>
2024-12-20 14:03:31 +1300

    fix: incorrectly applied css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -15,7 +15,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "api/config.ts";
 
 import "./preflight.css";
-import "./index.css";
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
