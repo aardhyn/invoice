06cedc46b188641657e4df3105f3bfaad7ccdd56
aardhynlavender <aardhyn@gmail.com>
2025-02-06 16:24:44 +1300

    fix: missing tables in revert migration script

--- server/repository/migrations/2024-07-25-225105_initial/down.sql
+++ server/repository/migrations/2024-07-25-225105_initial/down.sql
@@ -2,3 +2,8 @@ drop table business cascade;
 drop table location cascade;
 drop table payment;
 drop table contact;
+drop table client cascade;
+drop table service cascade;
+drop table product cascade;
+drop table invoice cascade;
+drop table invoice_template;
