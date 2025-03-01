c0fd6c7a52b111a51b2a1e99c4f781feb152be37
aardhynlavender <aardhyn@gmail.com>
2025-03-01 17:40:49 +1300

    build: specify react version

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,15 +6,16 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-  },
-  {
+    settings: {
+      react: { version: "detect" },
+    },
     languageOptions: {
       globals: globals.browser,
     },
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
