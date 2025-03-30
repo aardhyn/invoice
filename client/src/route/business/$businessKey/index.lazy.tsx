7b1c0e5f11c5d77a5fde77b4b374c67e91a68b72
aardhynlavender <aardhyn@gmail.com>
2025-03-30 20:36:04 +1300

    chore: cleanup business dashboard

--- client/src/route/business/$businessKey/index.lazy.tsx
+++ client/src/route/business/$businessKey/index.lazy.tsx
@@ -1,20 +1,24 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useBusinessGetQuery } from "api/business/get";
-import { Json } from "component";
+import { useBusinessGetQuery } from "api";
+import { Card, Code, H2, Section } from "component";
 
 export const Route = createLazyFileRoute("/business/$businessKey/")({
   component: Page,
 });
 
 function Page() {
-  const { businessKey } = Route.useParams();
-  const businessId = parseInt(businessKey);
+  const params = Route.useParams();
+  const businessId = parseInt(params.businessKey);
 
   const { data: business } = useBusinessGetQuery({ businessId });
 
   return (
-    <div>
-      <Json>{business}</Json>
-    </div>
+    <Section>
+      <Card>
+        <H2>Dashboard</H2>
+        <Code language="json">{params}</Code>
+        <Code language="json">{business}</Code>
+      </Card>
+    </Section>
   );
 }
